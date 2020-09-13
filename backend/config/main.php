19ad8324f7dbf5fc0d1e812a7158508018021269
m.emara <muhammad.emara@gmail.com>
2020-09-13 18:27:30 +0400

    Changing the layout using bootstrap 4

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,8 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+	'name' => 'VideoTube',
+	'language' => 'en-US',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,16 +1,17 @@
 <?php
-
-/* @var $this \yii\web\View */
-/* @var $content string */
-
-use backend\assets\AppAsset;
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use common\widgets\Alert;
-
-AppAsset::register($this);
+	
+	/* @var $this \yii\web\View */
+	
+	/* @var $content string */
+	
+	use backend\assets\AppAsset;
+	use yii\helpers\Html;
+	use yii\bootstrap4\Nav;
+	use yii\bootstrap4\NavBar;
+	use yii\widgets\Breadcrumbs;
+	use common\widgets\Alert;
+	
+	AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -19,50 +20,56 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
+	<?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+	<?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
+	<?php
+		NavBar::begin([
+			'brandLabel' => Yii::$app->name,
+			'brandUrl' => Yii::$app->homeUrl,
+			/* 'options' => [
+				 'class' => 'navbar-inverse navbar-fixed-top',
+			 ],*/
+		]);
+		$menuItems = [
+			['label' => 'Home', 'url' => ['/site/index']],
+		];
+		if (Yii::$app->user->isGuest) {
+			$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+		} else {
+			$menuItems[] = [
+				'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+				'url' => ['/site/logout'],
+				'linkOptions' => [
+					'data-method' => 'post'
+				]
+			]; /*'<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link logout']
             )
             . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+            . '</li>';*/
+		}
+		echo Nav::widget([
+			'options' => ['class' => 'navbar-nav navbar-right'],
+			'items' => $menuItems,
+		]);
+		NavBar::end();
+	?>
 
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+		<?= Breadcrumbs::widget([
+			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+		]) ?>
+		<?= Alert::widget() ?>
+		<?= $content ?>
     </div>
 </div>
 
